c99759b2193afaa3a1fb41ce595dd6728769b370
Mark Neal <neal.611@osu.edu>
2025-10-07 16:18:37 -0400

    Set up base React structure and routing for five initial pages

---
+++ vinylverse/src/components/confirmation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Confirmation = () => {
+
+    let title = "Confirmation page";
+
+    return (
+        <div>
+            <h1> 
+                {title}
+            </h1>
+        </div>
+    );
+};
+
+export default Confirmation;
