c186c3293cfbd2dbe13dfc81d6a81a8b3fa71337
snallapati.1 <shivaramnallapati@gmail.com>
2025-10-08 18:38:26 -0400

    Finished step 6

--- vinylverse/src/components/shippingEntry.js
+++ vinylverse/src/components/shippingEntry.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const shippingEntry = () => {
+const ShippingEntry = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    let title = "shipping entry page";
+  const [shipping, setShipping] = useState({
+    name: "",
+    addressLine1: "",
+    addressLine2: "",
+    city: "",
+    state: "",
+    zip: ""
+  });
 
-    return (
-        <div>
-            <h1> 
-                {title}
-            </h1>
-        </div>
-    );
+  const handleChange = (e) => {
+    setShipping({ ...shipping, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/purchase/viewOrder", { state: { ...location.state, shipping } });
+  };
+
+  return (
+    <div>
+      <h2>Enter Shipping Information</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input name="name" onChange={handleChange} value={shipping.name} required />
+
+        <label>Address Line 1:</label>
+        <input name="addressLine1" onChange={handleChange} value={shipping.addressLine1} required />
+
+        <label>Address Line 2:</label>
+        <input name="addressLine2" onChange={handleChange} value={shipping.addressLine2} />
+
+        <label>City:</label>
+        <input name="city" onChange={handleChange} value={shipping.city} required />
+
+        <label>State:</label>
+        <input name="state" onChange={handleChange} value={shipping.state} required />
+
+        <label>ZIP:</label>
+        <input name="zip" onChange={handleChange} value={shipping.zip} required />
+
+        <button type="submit">Next</button>
+      </form>
+    </div>
+  );
 };
 
-export default shippingEntry;
+export default ShippingEntry;
